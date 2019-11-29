b66d7c83eda964c160df5118f2d724381c1cbbed
Michael Sorokine <mssorokine@gmail.com>
2019-11-29 16:16:37 +0300

    add comment model, add request redirect + some major improvements

--- webapp/user/views.py
+++ webapp/user/views.py
@@ -1,5 +1,6 @@
 from webapp.user.forms import LoginForm, RegistrationForm
 from webapp.user.models import User
+from webapp.utils import get_redirect_target
 
 from webapp.db import db 
 
@@ -11,7 +12,7 @@ blueprint = Blueprint('user', __name__, url_prefix='/users')
 @blueprint.route('/login')
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('news.index'))
+        return redirect(get_redirect_target())
 
     title = "Authorization"
     login_form = LoginForm()
@@ -25,7 +26,7 @@ def process_login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             flash('Вы успешно вошли на сайт')
-            return redirect(url_for('news.index'))
+            return redirect(get_redirect_target())
     flash('Неправильное имя пользователя или пароль')
     return redirect(url_for('user.login'))
 
